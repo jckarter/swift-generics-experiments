563592d2e8ec11cc3cd72088a38715f48d781787
Joe Groff <jgroff@apple.com>
2016-04-02 11:22:54 -0700

    Initial Commit

---
+++ generics/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  generics
+//
+//  Created by Joe Groff on 4/2/16.
+//  Copyright © 2016 Joe Groff. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
